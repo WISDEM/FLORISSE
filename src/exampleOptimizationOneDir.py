5170f1e4298ef646942acaf88fcaa8b474a85de5
Jared J. Thomas <jaredthomas68@gmail.com>
2016-01-27 10:49:34 -0700

    removed extraneous lines from exampleOptimizationOneDir.py

--- src/exampleOptimizationOneDir.py
+++ src/exampleOptimizationOneDir.py
@@ -11,13 +11,8 @@ if __name__ == "__main__":
     rotor_diameter = 126.4  # (m)
 
     # define turbine locations in global reference frame
-    # original example case
-    # turbineX = np.array([1164.7, 947.2,  1682.4, 1464.9, 1982.6, 2200.1])   # m
-    # turbineY = np.array([1024.7, 1335.3, 1387.2, 1697.8, 2060.3, 1749.7])   # m
-    # Scaling grid case
     nRows = 3       # number of rows and columns in grid
     spacing = 5     # turbine grid spacing in diameters
-    # Set up position arrays
     points = np.linspace(start=spacing*rotor_diameter, stop=nRows*spacing*rotor_diameter, num=nRows)
     xpoints, ypoints = np.meshgrid(points, points)
     turbineX = np.ndarray.flatten(xpoints)
