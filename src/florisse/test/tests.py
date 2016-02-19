35f9de0630f77a932d420b9ecbb25d3b432474ac
Jared J. Thomas <jaredthomas68@gmail.com>
2016-02-19 12:35:19 -0700

    added total derivative checks

--- src/florisse/test/tests.py
+++ src/florisse/test/tests.py
@@ -1,10 +1,109 @@
 import unittest
 # from florisse.GeneralWindFarmComponents import *
+from openmdao.api import pyOptSparseDriver
 from florisse.floris import *
-# from florisse.OptimizationGroups import *
+from florisse.OptimizationGroups import *
 from _floris import *
 
 import cPickle as pickle
+
+
+class TotalDerivTestsFlorisAEPOpt(unittest.TestCase):
+
+    def setUp(self):
+
+        nTurbines = 4
+        self.atol = 1E-3
+        self.rtol = 1E-6
+
+        np.random.seed(seed=5)
+
+        turbineX = np.random.rand(nTurbines)*3000.
+        turbineY = np.random.rand(nTurbines)*3000.
+
+        minSpacing = 2.
+
+        # initialize input variable arrays
+        rotorDiameter = np.ones(nTurbines)*np.random.random()*150.
+        axialInduction = np.ones(nTurbines)*np.random.random()*(1./3.)
+        Ct = np.ones(nTurbines)*np.random.random()
+        Cp = np.ones(nTurbines)*np.random.random()
+        generator_efficiency = np.ones(nTurbines)*np.random.random()
+        yaw = np.random.rand(nTurbines)*60. - 30.
+
+        # Define flow properties
+        nDirections = 50.0
+        windSpeeds = np.random.rand(nDirections)*20        # m/s
+        air_density = 1.1716    # kg/m^3
+        windDirections = np.random.rand(nDirections)*360.0
+        windFrequencies = np.random.rand(nDirections)
+
+        # set up problem
+        # prob = Problem(root=OptAEP(nTurbines, nDirections=1, resolution=0))
+
+        prob = Problem(root=OptAEP(nTurbines=nTurbines, nDirections=windDirections.size, resolution=0,
+                                          minSpacing=minSpacing))
+
+        # set up optimizer
+        # prob.driver = pyOptSparseDriver()
+        # prob.driver.options['optimizer'] = 'SNOPT'
+        prob.driver.add_objective('obj', scaler=1E-8)
+
+        # set optimizer options
+        # prob.driver.opt_settings['Verify level'] = 3
+        # prob.driver.opt_settings['Print file'] = 'SNOPT_print_exampleOptAEP.out'
+        # prob.driver.opt_settings['Summary file'] = 'SNOPT_summary_exampleOptAEP.out'
+        # prob.driver.opt_settings['Major iterations limit'] = 1
+
+        # select design variables
+        prob.driver.add_desvar('turbineX', lower=np.ones(nTurbines)*min(turbineX), upper=np.ones(nTurbines)*max(turbineX), scaler=1E-2)
+        prob.driver.add_desvar('turbineY', lower=np.ones(nTurbines)*min(turbineY), upper=np.ones(nTurbines)*max(turbineY), scaler=1E-2)
+        for direction_id in range(0, windDirections.size):
+            prob.driver.add_desvar('yaw%i' % direction_id, lower=-30.0, upper=30.0, scaler=1E-1)
+
+        # add constraints
+        prob.driver.add_constraint('sc', lower=np.zeros(((nTurbines-1.)*nTurbines/2.)))
+
+        # initialize problem
+        prob.setup()
+
+        # assign values to constant inputs (not design variables)
+        prob['turbineX'] = turbineX
+        prob['turbineY'] = turbineY
+        prob['yaw0'] = yaw
+        prob['rotorDiameter'] = rotorDiameter
+        prob['axialInduction'] = axialInduction
+        prob['Ct_in'] = Ct
+        prob['Cp_in'] = Cp
+        prob['generator_efficiency'] = generator_efficiency
+        prob['windSpeeds'] = windSpeeds
+        prob['air_density'] = air_density
+        prob['windDirections'] = windDirections
+        prob['windrose_frequencies'] = windFrequencies
+        prob['floris_params:FLORISoriginal'] = True
+
+        # run problem
+        prob.run()
+
+        # pass results to self for use with unit test
+        self.J = prob.check_total_derivatives()
+        self.nDirections = nDirections
+
+        # print self.J
+
+    def testObj(self):
+
+        np.testing.assert_allclose(self.J[('obj', 'turbineX')]['rel error'], self.J[('obj', 'turbineX')]['rel error'], self.atol, self.rtol)
+        np.testing.assert_allclose(self.J[('obj', 'turbineY')]['rel error'], self.J[('obj', 'turbineY')]['rel error'], self.atol, self.rtol)
+        for dir in np.arange(0, self.nDirections):
+            np.testing.assert_allclose(self.J[('obj', 'yaw%i' % dir)]['rel error'], self.J[('obj', 'yaw%i' % dir)]['rel error'], self.atol, self.rtol)
+
+    def testCon(self):
+
+        np.testing.assert_allclose(self.J[('sc', 'turbineX')]['rel error'], self.J[('sc', 'turbineX')]['rel error'], self.atol, self.rtol)
+        np.testing.assert_allclose(self.J[('sc', 'turbineY')]['rel error'], self.J[('sc', 'turbineY')]['rel error'], self.atol, self.rtol)
+        for dir in np.arange(0, self.nDirections):
+            np.testing.assert_allclose(self.J[('sc', 'yaw%i' % dir)]['rel error'], self.J[('sc', 'yaw%i' % dir)]['rel error'], self.atol, self.rtol)
 
 
 class GradientTestsFLORIS(unittest.TestCase):
