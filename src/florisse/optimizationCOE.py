ce07031e1df502827d3d126609c64e23a22fb46c
PJ Stanley <stanley_andrewpj@yahoo.com>
2016-06-03 09:41:14 -0600

    alternates tall and short turbines

--- src/florisse/optimizationCOE.py
+++ src/florisse/optimizationCOE.py
@@ -106,28 +106,72 @@ if __name__ == "__main__":
 
     # Scaling grid case
     # nRows = int(sys.argv[1])     # number of rows and columns in grid
-    nRows = 6
+    nRows = 5
     spacing = 3    # turbine grid spacing in diameters
 
     # Set up position arrays
     points = np.linspace(start=spacing*rotor_diameter, stop=nRows*spacing*rotor_diameter, num=nRows)
     xpoints, ypoints = np.meshgrid(points, points)
+    """
     turbineX = np.ndarray.flatten(xpoints)
     turbineY = np.ndarray.flatten(ypoints)
-    """turbineX1 = np.array([379.2,1137.6,1896,758.4,1516.8,379.2,1137.6,1896,758.4,1516.8,379.2,1137.6,1896])
+    """
+    """
+    turbineX1 = np.array([379.2,1137.6,1896,758.4,1516.8,379.2,1137.6,1896,758.4,1516.8,379.2,1137.6,1896])
     turbineX2 = np.array([758.4,1516.8,379.2,1137.6,1896,758.4,1516.8,379.2,1137.6,1896,758.4,1516.8])
     turbineX = np.hstack([turbineX1,turbineX2])
     turbineY1 = np.array([379.2,379.2,379.2,758.4,758.4,1137.6,1137.6,1137.6,1516.8,1516.8,1896,1896,1896])
     turbineY2 = np.array([379.2,379.2,758.4,758.4,758.4,1137.6,1137.6,1516.8,1516.8,1516.8,1896,1896])
-    turbineY = np.hstack([turbineY1,turbineY2])"""
+    turbineY = np.hstack([turbineY1,turbineY2])
+    """
 
+    
+    inverted = "no"
+    
+
+    nRows = 5
+    spacing = 3   # turbine grid spacing in diameters
+    nTurbs = nRows*nRows
+    # Set up position arrays
+    rotor_diameter = 126.4 
+    points = np.linspace(start=spacing*rotor_diameter, stop=nRows*spacing*rotor_diameter, num=nRows)
+    xpoints, ypoints = np.meshgrid(points, points)
+    
+    turbineX = np.ndarray.flatten(xpoints)
+    turbineY = np.ndarray.flatten(ypoints)
+    turbineX1 = np.array([])
+    turbineX2 = np.array([])
+    turbineY1 = np.array([])
+    turbineY2 = np.array([])
+
+    for i in range(nTurbs):
+        if i%2 == 0:
+            turbineX1 = np.append(turbineX1, turbineX[i])
+            turbineY1 = np.append(turbineY1, turbineY[i])
+        else:
+            turbineX2 = np.append(turbineX2, turbineX[i])
+            turbineY2 = np.append(turbineY2, turbineY[i])
+
+    if inverted == "yes":
+        turbineX = np.hstack([turbineX2,turbineX1])
+        turbineY = np.hstack([turbineY2,yturbineY1])
+    else:
+        turbineX = np.hstack([turbineX1,turbineX2])
+        turbineY = np.hstack([turbineY1,turbineY2])
+
+
+
+    print('Turbine X', turbineX)
+    print('Turbine Y', turbineY)
     # initialize input variable arrays
     nTurbs = turbineX.size
     #87.6m is the height of the 5MW NREL turbines
-    turbineH1 = 90
-    turbineH2 = 150
+    turbineMin = 70
+    turbineH1 = 130
+    turbineH2 = 110
     #turbineH2 = 87.6
-    nTurbsH1 = 18
+    #nTurbsH1 = 5
+    nTurbsH1 = len(turbineX1)
     nTurbsH2 = nTurbs-nTurbsH1
     rotorDiameter = np.zeros(nTurbs)
     axialInduction = np.zeros(nTurbs)
@@ -199,8 +243,8 @@ if __name__ == "__main__":
     print('nturbsH2: ', nTurbsH2)
     prob.driver.add_desvar('turbineX', lower=np.ones(nTurbs)*min(turbineX), upper=np.ones(nTurbs)*max(turbineX), scaler=1E-2)
     prob.driver.add_desvar('turbineY', lower=np.ones(nTurbs)*min(turbineY), upper=np.ones(nTurbs)*max(turbineY), scaler=1E-2)
-    prob.driver.add_desvar('turbineH1', 87.6, upper=None, scaler=1E-2)
-    prob.driver.add_desvar('turbineH2', 87.6, upper=None, scaler=1E-2)
+    prob.driver.add_desvar('turbineH1', turbineMin, upper=175, scaler=1E-2)
+    prob.driver.add_desvar('turbineH2', turbineMin, upper=175, scaler=1E-2)
     # prob.driver.add_desvar('turbineZ', lower=np.ones(nTurbs)*100., upper=np.ones(nTurbs)*500., scaler=1E-2)
     """for direction_id in range(0, windDirections.size):
         prob.driver.add_desvar('yaw%i' % direction_id, lower=-30.0, upper=30.0, scaler=1E-1)"""
@@ -274,19 +318,19 @@ if __name__ == "__main__":
     optCOE = prob['COE']
     optAEP = prob['AEP']
 
-    #np.savetxt('XY.txt',np.c_[prob['turbineX'],prob['turbineY'],prob['turbineZ']])
+    np.savetxt('XYZ5test.txt',np.c_[prob['turbineX'],prob['turbineY'],prob['turbineZ']])
 
     xbounds = [min(turbineX), min(turbineX), max(turbineX), max(turbineX), min(turbineX)]
     ybounds = [min(turbineY), max(turbineY), max(turbineY), min(turbineY), min(turbineX)]
 
-    """plt.figure()
+    plt.figure(1)
     #plt.plot(turbineX, turbineY, 'ok', label='Original')
     plt.plot(turbineX1, turbineY1, '.k', label='Short Original')
     for i in range(0, nTurbsH1):
-        plt.plot(prob['turbineX'][i],prob['turbineY'][i], 'ok', label='Short Optimized')
+        plt.plot(prob['turbineX'][i],prob['turbineY'][i], 'ok')
     plt.plot(turbineX2, turbineY2, '.g', label='Tall Original')
     for i in range(nTurbsH1, nTurbsH1+nTurbsH2):
-        plt.plot(prob['turbineX'][i],prob['turbineY'][i], 'og', label='Tall Optimized')
+        plt.plot(prob['turbineX'][i],prob['turbineY'][i], 'og')
     #plt.plot(prob['turbineX'], prob['turbineY'], 'og', label='Optimized')
     plt.plot(xbounds, ybounds, ':k')
     for i in range(0, nTurbs):
@@ -295,7 +339,7 @@ if __name__ == "__main__":
     plt.xlabel('Turbine X Position (m)')
     plt.ylabel('Turbine Y Position (m)')
     plt.legend(bbox_to_anchor=(1.14, 1.14))
-    plt.show()"""
+    plt.show() 
 
     ##########################################################################################
     ##########################################################################################
@@ -303,8 +347,8 @@ if __name__ == "__main__":
     #Only modify x and y
 
 
-    turbineH1 = 87.6
-    turbineH2 = 87.6
+    turbineH1 = turbineMin
+    turbineH2 = turbineMin
 
     # set up problem
     prob = Problem()
@@ -332,7 +376,7 @@ if __name__ == "__main__":
     # set up optimizer
     prob.driver = pyOptSparseDriver()
     prob.driver.options['optimizer'] = 'SNOPT'
-    prob.driver.add_objective('maxAEP', scaler=1E-2)
+    prob.driver.add_objective('maxAEP', scaler=1E-6)
 
     # set optimizer options
     prob.driver.opt_settings['Verify level'] = 3
@@ -420,12 +464,12 @@ if __name__ == "__main__":
 
     tdCOE = prob['COE']
     tdAEP = prob['AEP']
-    #np.savetxt('XY.txt',np.c_[prob['turbineX'],prob['turbineY'],prob['turbineZ']])
+    np.savetxt('XY5test.txt',np.c_[prob['turbineX'],prob['turbineY'],prob['turbineZ']])
 
     xbounds = [min(turbineX), min(turbineX), max(turbineX), max(turbineX), min(turbineX)]
     ybounds = [min(turbineY), max(turbineY), max(turbineY), min(turbineY), min(turbineX)]
 
-    plt.figure()
+    plt.figure(2)
     plt.plot(turbineX, turbineY, 'ok', label='Original')
     plt.plot(prob['turbineX'], prob['turbineY'], 'og', label='Optimized')
     plt.plot(xbounds, ybounds, ':k')
@@ -435,7 +479,7 @@ if __name__ == "__main__":
     plt.xlabel('Turbine X Position (m)')
     plt.ylabel('Turbine Y Position (m)')
     plt.legend(bbox_to_anchor=(1.14, 1.14))
-    plt.show() 
-
     perDec = (tdCOE-optCOE)/tdCOE*100
     print('Percent Decrease: ', perDec)
+    plt.show() 
+
